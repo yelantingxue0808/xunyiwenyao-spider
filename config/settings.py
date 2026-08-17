2bd9f9abb2324bd0ad14bd5fb8dfb589e6791e24
yelantingxue0808 <2511379054@qq.com>
2026-08-17 18:10:49 +0800

    配置层：请求、cookie等反爬参数

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+HEADERS = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36 Edg/145.0.0.0",
+        'referer': 'https://club.xywy.com/list_all_1000.htm',
+        'cookie': 'clientac=1773579872744100054; visit_dt=2026-2-15; xywylastUrl=https%253A%252F%252Fclub.xywy.com%252F'
+                  'list_all_1000.htm; xywylastRef=https%253A%252F%252Fclub.xywy.com%252Flist_all_2.htm; XYWYDATAxywy=17'
+                  '73579872-2089199966@177357987274124788223140@41; XYWYDATADAYxywy=1773579872-1773590400@41; XYWYDATASE'
+                  'SSIONxywy=''1773579872741654242@41@1773580766@5; ajsDataSession=177357987274447155@43@1773580767@5; '
+                  'tj_lastUrl=https%3A//club.xywy.com/list_all_1000.htm; tj_lastUrl_time=1773580767023'
+
+    }
